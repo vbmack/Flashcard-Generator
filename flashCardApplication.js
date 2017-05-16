8694f37f20505783185c9963d1e8d137272ab045
Mack <vbmack@uncc.edu>
2017-05-16 15:32:24 -0400

    flash-card generator

---
+++ flashCardApplication.js
@@ -0,0 +1,86 @@
+// Require the 'inquirer' package
+var inquirer = require('inquirer');
+
+// Import the flash cards constructor implementations
+var flashCards = require('./flashCards.js');
+// Import the full list of questions
+var questions = require('./questions.js').questions;
+
+// Variable that holds the cloze-deleted questions list
+var closeQuestions = [];
+
+// Populate the cloze-deleted questions list
+for (var i = 0; i < questions.length; i++) {
+	var q = new flashCards.ClozeCard(questions[i].full, questions[i].cloze);
+	closeQuestions.push(q);
+}
+
+// What question the user is currently on
+var currentQuestion = 0;
+// How many questions the user has gotten right
+var answerRight = 0;
+// How many questions the user has gotten wrong
+var answerWrong = 0;
+
+// askQuestion prompts the user to answer a given cloze-deleted question
+function askQuestion() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			message: closeQuestions[currentQuestion].partial + '\nAnswer: ',
+			name: 'userGuess'
+		}
+	]).then(function (answers) {
+		console.log('\n');
+
+		// Check if the user has guessed correctly
+		if (answers.userGuess.toLowerCase() === closeQuestions[currentQuestion].cloze.toLowerCase()) {
+			console.log('Correct!');
+			answerRight++;
+		} else {
+			console.log('Incorrect!');
+			answerWrong++;
+		}
+
+		// Show the correct answer
+		console.log(closeQuestions[currentQuestion].full);
+		console.log('-------------------------------------\n');
+
+		// Advance to the next question
+		if (currentQuestion < closeQuestions.length - 1) {
+			currentQuestion++;
+			askQuestion();
+		} else {
+			console.log('Game Over!');
+			console.log('Correct Answers: ' + answerRight);
+			console.log('Incorrect Answers: ' + answerWrong);
+
+			console.log('-------------------------------------\n');
+
+			// Prompt the user to play again
+			inquirer.prompt([
+				{
+					type: 'confirm',
+					message: 'Would you like to play again?',
+					name: 'playAgain'
+				}
+			]).then(function (answers) {
+				if (answers.playAgain) {
+					// Reset the game
+					currentQuestion = 0;
+					answerRight = 0;
+					answerWrong = 0;
+
+					// Begin asking the questions!
+					askQuestion();
+				} else {
+					// Exit the game
+					console.log('Thanks for playing! Goodbye!');
+				}
+			})
+		}
+	})
+}
+
+// Begin asking the questions!
+askQuestion();
---
+++ questions.js
@@ -0,0 +1,74 @@
+exports.questions = [
+	{
+		full: 'DJ Kool Herc is the founding father of hip hop.',
+		cloze: 'DJ Kool Herc'
+	},
+	{
+		full: 'The number 1 hip hop album in sales is OutKast Speakerboxxx/The Love Below.',
+		cloze: 'Speakerboxxx/The Love Below'
+	},
+	{
+		full: 'This movie, Krush Groove, starred LL Cool J, RUN DMC, and Sheila E.',
+		cloze: 'Krush Groove'
+	},
+	{
+		full: 'Doug E. Fresh in hip hop is known as the worlds greatest entertainer.',
+		cloze: 'Doug E. Fresh'
+	},
+	{
+		full: 'Marshall Mathers on-stage name is Eminem.',
+		cloze: 'Eminem'
+	},
+	{
+		full: 'Dr. Dre, aka Andre Young, is founding member of NWA.',
+		cloze: 'Dr. Dre'
+	},
+	{
+		full: 'Snoop Dogg made his first movie appearance in Half Baked.',
+		cloze: 'Half Baked'
+	},
+	{
+		full: 'KRS-One collaborated with the late Scott-La-Rock to create the legendary Criminal Minded album.',
+		cloze: 'Criminal Minded'
+	},
+	{
+		full: 'MC Shan believed that hip hop started in the Bridge.',
+		cloze: 'Bridge'
+	},
+	{
+		full: 'The late Trouble T Roy was a dancer for Heavy D and the Boyz.',
+		cloze: 'Trouble T Roy'
+	},
+	{
+		full: 'Rapper Notorius BIG often referred to himself as Frank White, the main character from the movie King of New York.',
+		cloze: 'Frank White'
+	},
+	{
+		full: 'Lil Kim was the only female member of Junior Mafia',
+		cloze: 'Junior Mafia'
+	},
+	{
+		full: 'The only female member of Ruff Ryders was Eve',
+		cloze: 'Eve'
+	},
+	{
+		full: 'DMX starred with Nas in the hip hop classic movie, Belly.',
+		cloze: 'Belly'
+	},
+	{
+		full: 'P Diddy is the wealthiest rapper on Forbes 2017 list.',
+		cloze: 'P Diddy'
+	},
+	{
+		full: 'Vol 2 Hard Knock Life is Jay Zs most successful album.',
+		cloze: 'Jay Zs'
+	},
+	{
+		full: 'Tupac Shakur was killed in 1996.',
+		cloze: '1996'
+	},
+	{
+		full: 'Wonder Mike, Big Bank Hank, and Master Gee were members of the Sugarhill Gang.',
+		cloze: 'Sugarhill Gang'
+	}
+]
